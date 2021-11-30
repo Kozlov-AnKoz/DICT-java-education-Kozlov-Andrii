d84e2984a35deed668093fa4745fe4c17c655100
Andrey Kozlov <kozlov.for.work.mail@gmail.com>
2021-11-30 15:18:34 +0200

    Add Test.java file

--- Test.java
+++ Test.java
@@ -1,5 +1,11 @@
 public class Test {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Java");
     }
 }
+=======
+        System.out.println("education");
+    }
+}
+>>>>>>> 37b7128 (Add Test.java file)
